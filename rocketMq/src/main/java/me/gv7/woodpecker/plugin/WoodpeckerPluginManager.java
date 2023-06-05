608ddaff65592259f02675f5d2402c773d136cc7
余生 <ht22664957@163.com>
2023-06-05 17:29:24 +0800

    初始化项目

---
+++ rocketMq/src/main/java/me/gv7/woodpecker/plugin/WoodpeckerPluginManager.java
@@ -0,0 +1,18 @@
+package me.gv7.woodpecker.plugin;
+
+import me.gv7.woodpecker.vuldb.RocketMqInfo;
+
+/**
+ * @ClassName: WoodpeckerPluginManager
+ * @Description: TODO
+ * @Author: yz
+ * @Date: 2023-06-05
+ * @Version: v1.0
+ * @Description:
+ **/
+public class WoodpeckerPluginManager implements IPluginManager{
+    public void registerPluginManagerCallbacks(IPluginManagerCallbacks iPluginManagerCallbacks) {
+        final RocketMqInfo vulInfo = new RocketMqInfo();
+        iPluginManagerCallbacks.registerVulPlugin(vulInfo);
+    }
+}
---
+++ rocketMq/src/main/java/me/gv7/woodpecker/vuldb/RocketMqInfo.java
@@ -0,0 +1,18 @@
+package me.gv7.woodpecker.vuldb;
+
+import me.gv7.woodpecker.plugin.IVulPlugin;
+import me.gv7.woodpecker.plugin.IVulPluginCallbacks;
+
+/**
+ * @ClassName: VulPluginInfo
+ * @Description: TODO
+ * @Author: yz
+ * @Date: 2023-06-05
+ * @Version: v1.0
+ * @Description:
+ **/
+public class RocketMqInfo implements IVulPlugin {
+    public void VulPluginMain(IVulPluginCallbacks iVulPluginCallbacks) {
+
+    }
+}
